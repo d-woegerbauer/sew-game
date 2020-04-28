4efcf58512483818d265e37d2e9c4766aae0c6ee
ToyotaKakashi <43617783+ToyotaKakashi@users.noreply.github.com>
2020-04-28 17:06:12 +0200

    Added basic structure

---
+++ Projekt/desktop/src/tv/gregor/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tv.gregor.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tv.gregor.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
