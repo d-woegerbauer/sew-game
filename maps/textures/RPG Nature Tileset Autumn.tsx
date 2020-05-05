1f3318eb5160d752d8ead293c5fbf564d5c805aa
dav-prog <david.woegerb@gmail.com>
2020-05-05 16:52:45 +0200

    Added textures

---
+++ maps/textures/RPG Nature Tileset Autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="RPG Nature Tileset Autumn" tilewidth="16" tileheight="16" tilecount="540" columns="30">
+ <image source="RPG Nature Tileset Autumn.png" width="480" height="288"/>
+</tileset>
