4c759bee6220bddfa68f2e47fc9addde8102bb61
dav-prog <david.woegerb@gmail.com>
2020-05-26 15:53:38 +0200

    changed something

--- Projekt/core/assets/overworld.tsx
+++ Projekt/core/assets/overworld.tsx
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="overworld" tilewidth="16" tileheight="16" tilecount="152" columns="19">
- <image source="overworld.png" width="304" height="128"/>
-</tileset>
+<;?xml version="1.0" encoding="UTF-8"?>
+<;tileset; version="1.2"; tiledversion="1.3.4"; name="overworld"; tilewidth="16"; tileheight="16"; tilecount="152"; columns="19">
+ <image; source="overworld.png"; width="304"; height="128"/>
+</tileset>;
